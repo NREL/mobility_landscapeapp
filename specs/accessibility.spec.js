ccd8a962f7ed81e935bf669d92fa84443dc0bc83
Ben Burch <bburch@nrel.gov>
2020-12-08 13:44:05 -0500

    Enable integration testing as a part of the build process

--- specs/accessibility.spec.js
+++ specs/accessibility.spec.js
@@ -1,7 +1,10 @@
 import puppeteer from "puppeteer";
 const { AxePuppeteer } = require('axe-puppeteer');
 
-const appUrl = `http://localhost:${(process.env.PORT || '4000')}`;
+const prefix = process.env.ROUTE_BASE || '';
+const port = process.env.PORT || '4000';
+const appUrl = `http://localhost:${port}/${prefix}`;
+console.log("Connecting to URL "+appUrl);
 
 const analyzePage = async (url) => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], defaultViewport: { width: 1600, height: 1200 }});
